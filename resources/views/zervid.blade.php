7fde517bfc176638ac4302837bc06df1250bb440
channrong09 <59310455@go.buu.ac.th>
2020-06-01 22:06:36 +0700

    a

--- resources/views/zervid.blade.php
+++ resources/views/zervid.blade.php
@@ -10,12 +10,17 @@
 
 @section('content')
 <center>
-
-    <table width=90% style="border:1px dashed #00aa77;" cellspacing="5" bgcolor="#00aa77" cellpadding="5"><tr><td style="border:1px dashed white;"bgcolor="white">รายละเอียดงานสวนปาล์ม </td></tr></table>
+    <style>
+        h1 {text-align: center;}
+        p {text-align: center;}
+        div {text-align: center;}
+        </style>
+        <h1>รายละเอียดงานสวนปาล์ม </h1>
+    {{-- <table width=90% style="border:1px dashed #00aa77;" cellspacing="5" bgcolor="#00aa77" cellpadding="5"><tr><td style="border:10px dashed white;"bgcolor="white">รายละเอียดงานสวนปาล์ม </td></tr></table> --}}
 
     <table width=90% style="border:0px dashed #ffaacc;" cellspacing="0" bgcolor="#ffaacc" cellpadding="5"><tr>
     <td style="border:1px dashed white;" bgcolor="white"><img src="{{ asset('images/demo/backgrounds/a5.jpg') }}" height="400" width="400">
-        <table width=90% cellpadding="4" cellspacing="0" style="border:double 0px hotpink;"><tr><td style="border:double 5px hotpink;">1.บริการตัดปาล์ม คิดเป็นกิโลละ 3 บาท ตัดได้กี่กิโลกรัมเอามา * 3 (แล้วหักค่ามัดจำ 30% ) (อีก 70 % เป็นของลูกค้า)</td></tr></table>
+        <table width=90% cellpadding="4" cellspacing="0" style="border:double 0px rgb(105, 183, 255);"><tr><td style="border:double 5px hotpink;">1.บริการตัดปาล์ม คิดเป็นกิโลละ 3 บาท ตัดได้กี่กิโลกรัมเอามา * 3 (แล้วหักค่ามัดจำ 30% ) (อีก 70 % เป็นของลูกค้า)</td></tr></table>
         <br><br><br><br><br>
     <img src="{{ asset('images/demo/backgrounds/a7.jpg') }}" height="400" width="400">
     <table width=90% cellpadding="4" cellspacing="0" style="border:double 5px hotpink;"><tr><td style="border:double 5px hotpink;">2.บริการตัดหญ้า จำนวนไร่ละ 500 (ค่าแรง 400 + 100 ค่าน้ำมัน)</td></tr></table>
