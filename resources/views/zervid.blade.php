9ff857a6c0815edbf028adac54aaa8eef4a86ff6
channrong09 <59310455@go.buu.ac.th>
2020-06-06 15:28:19 +0700

    กห

--- resources/views/zervid.blade.php
+++ resources/views/zervid.blade.php
@@ -4,7 +4,7 @@
 {{-- <div class="bgded overlay" style="background-image:url('{{asset('./images/demo/backgrounds/aa.jpg')}}');"> --}}
 
 <style>
-    body { background-color:#00aa77; }
+    body { background-color:#e2e2da; }
 </style>
 @endsection
 
@@ -18,13 +18,39 @@
         <h1>รายละเอียดงานสวนปาล์ม </h1>
     {{-- <table width=90% style="border:1px dashed #00aa77;" cellspacing="5" bgcolor="#00aa77" cellpadding="5"><tr><td style="border:10px dashed white;"bgcolor="white">รายละเอียดงานสวนปาล์ม </td></tr></table> --}}
 
-    <table width=90% style="border:0px dashed #ffaacc;" cellspacing="0" bgcolor="#ffaacc" cellpadding="5"><tr>
+    {{-- <table width=90% style="border:0px dashed #ffaacc;" cellspacing="0" bgcolor="#ffaacc" cellpadding="5"><tr> --}}
+        <table>
+            <thead>
+                <tr>
+                    <h2>1.บริการตัดปาล์ม </h2>
+                </tr>
+
+            </thead>
+         </table>
     <td style="border:1px dashed white;" bgcolor="white"><img src="{{ asset('images/demo/backgrounds/a5.jpg') }}" height="400" width="400">
-        <table width=90% cellpadding="4" cellspacing="0" style="border:double 0px rgb(105, 183, 255);"><tr><td style="border:double 5px rgb(33, 235, 43);">1.บริการตัดปาล์ม คิดเป็นกิโลละ 3 บาท ตัดได้กี่กิโลกรัมเอามา * 3 (แล้วหักค่ามัดจำ 30% ) (อีก 70 % เป็นของลูกค้า)</td></tr></table>
+        <table width=90% cellpadding="4" cellspacing="0" style="border:double 0px rgb(105, 183, 255);"><tr><td style="border:double 5px rgb(33, 235, 43);">1.บริการตัดปาล์ม คิดเป็นกิโลละ 3 บาท  แล้วหักค่ามัดจำ 30% </td></tr></table>
         <br><br><br><br><br>
+        <table>
+            <thead>
+                <tr>
+                    <h2>2.บริการใส่ปุ๋ย </h2>
+                </tr>
+
+            </thead>
+         </table>
+
     <img src="{{ asset('images/demo/backgrounds/a7.jpg') }}" height="400" width="400">
     <table width=90% cellpadding="4" cellspacing="0" style="border:double 5px rgb(19, 196, 13);"><tr><td style="border:double 5px rgb(36, 235, 36);">2.บริการตัดหญ้า จำนวนไร่ละ 500 (ค่าแรง 400 + 100 ค่าน้ำมัน)</td></tr></table>
         <br><br><br><br><br>
+
+        <table>
+            <thead>
+                <tr>
+                    <h2>3.บริการตัดหญ้า </h2>
+                </tr>
+
+            </thead>
+         </table>
     <img src="{{ asset('images/demo/backgrounds/a6.jpg') }}" height="400" width="400">
     <table width=90% cellpadding="4" cellspacing="0" style="border:double 0px rgb(25, 228, 42);"><tr><td style="border:double 5px rgb(31, 231, 64);">3.บริการใส่ปุ๋ย คิดเป็นกระสอบละ 50 กิโลกรัม ปุ๋ย 1 กระสอบราคา 600  บาท  1 กระสอบใส่ได้ 50 ต้น</td></tr></table>
 </center>
